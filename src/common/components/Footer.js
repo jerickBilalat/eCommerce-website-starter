e0e66c64a02c43a921db624ce69cbd36f9f871dd
Jerick Bilalat <jrk.mb.xm@gmail.com>
2019-08-28 19:15:21 -0500

    add global styles, themes, fonts, and render footer to test and also add react-toastify

---
+++ src/common/components/Footer.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const footer = () => {
+  return (
+    <div id="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-5 col-sm-6 col-xs-12">
+            <h4>About</h4>
+            <p>
+              Morbi convallis bibendum urna ut viverra. Maecenas quis consequat libero, a feugiat
+              eros. Nunc ut lacinia tortor morbi ultricies laoreet ullamcorper phasellus semper.
+            </p>
+            <a href="facebook.com" className="button social-btn">
+              <i className="fa fa-facebook-official" />
+              Like Us on Facebook
+            </a>
+          </div>
+
+          <div className="col-md-4  col-sm-6 col-xs-12">
+            <h4>Helpful Links</h4>
+            <ul className="footer-links">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/">Shop</Link>
+              </li>
+              <li>
+                <Link to="/cart">Cart</Link>
+              </li>
+            </ul>
+
+            <ul className="footer-links">
+              <li>
+                <a href="http://www.jerickbilalat.com">Portfolio</a>
+              </li>
+              <li>
+                <a href="http://www.jerickbilalat.com">Blog</a>
+              </li>
+              <li>
+                <a href="http://www.jerickbilalat.com">Contact</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-md-3  col-sm-12 col-xs-12">
+            <h4>Contact Us</h4>
+            <div className="text-widget">
+              <span>12345 Little Lonsdale St, Melbourne</span>
+              <br />
+              Phone:
+              <span>(123) 123-456 </span>
+              <br />
+              Fax:
+              <span>(123) 123-456</span>
+              <br />
+              E-Mail:
+              <span> office@example.com </span>
+              <br />
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-12">
+            <div className="copyrights">© Copyright 2019 by JRK. All Rights Reserved.</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default footer;
--- src/common/containers/App.js
+++ src/common/containers/App.js
@@ -1,43 +1,48 @@
 import React, { Fragment } from 'react';
 import { renderRoutes } from 'react-router-config';
 import PropTypes from 'prop-types';
-// import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import ErrorBoundary from '../components/ErrorBoundry';
-// import Footer from './components/Footer';
+import Footer from '../components/Footer';
+
+import 'react-toastify/dist/ReactToastify.min.css';
+import '../assets/css/style.css';
+import '../assets/css/blue.css';
+
 
 class App extends React.Component {
-  // notify = (status, message) => {
-  //   switch (status) {
-  //     case 'success':
-  //       return toast.success(message, {
-  //         position: toast.POSITION.TOP_RIGHT
-  //       });
-  //     case 'error':
-  //       return toast.error(message, {
-  //         position: toast.POSITION.TOP_RIGHT
-  //       });
-  //     case 'info':
-  //       return toast.info(message, {
-  //         position: toast.POSITION.BOTTOM_LEFT
-  //       });
-  //     case 'warn':
-  //       return toast.warn(message, {
-  //         position: toast.POSITION.BOTTOM_RIGHT
-  //       });
-  //     default:
-  //       return toast('Default Notification !');
-  //   }
-  // };
+  notify = (status, message) => {
+    switch (status) {
+      case 'success':
+        return toast.success(message, {
+          position: toast.POSITION.TOP_RIGHT
+        });
+      case 'error':
+        return toast.error(message, {
+          position: toast.POSITION.TOP_RIGHT
+        });
+      case 'info':
+        return toast.info(message, {
+          position: toast.POSITION.BOTTOM_LEFT
+        });
+      case 'warn':
+        return toast.warn(message, {
+          position: toast.POSITION.BOTTOM_RIGHT
+        });
+      default:
+        return toast('Default Notification !');
+    }
+  };
 
   render() {
     const { route } = this.props;
     return (
       <Fragment>
-        {/* <ToastContainer /> */}
+        <ToastContainer />
         <div className="container">
           <ErrorBoundary>{renderRoutes(route.routes)}</ErrorBoundary>
         </div>
-        {/* <Footer /> */}
+        <Footer />
       </Fragment>
     );
   }
