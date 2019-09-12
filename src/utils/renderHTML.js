f498808654b01534b9569af503ddda2e7b2db677
Jerick Bilalat <jrk.mb.xm@gmail.com>
2019-09-12 08:11:38 -0500

    edit generic title

--- src/utils/renderHTML.js
+++ src/utils/renderHTML.js
@@ -24,7 +24,7 @@ export default function(req, store, context) {
       <head>
           <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta charSet='utf-8' />
-          <title>Razzle Redux Example</title>
+          <title>eCommerce Website Starter</title>
           <meta name="viewport" content="width=device-width, initial-scale=1">
           ${assets.client.css ? `<link rel="stylesheet" href="${assets.client.css}">` : ''}
             ${
